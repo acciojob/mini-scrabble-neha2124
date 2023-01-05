ea7456c938973cd74f8db36b7f3716dbce6f569c
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-05 21:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
 let text = document.getElementById("evaluatedText")
-let countNum = document.getElementById("count")
+let countNum = document.getElementById("letterCount")
 text.addEventListener("input" , (e) => {
 	let val = e.target.value
 	let count = val.length;
